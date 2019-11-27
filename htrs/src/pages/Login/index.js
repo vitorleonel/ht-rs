dec7e71f4afa2395fc0e7d1ecaffe55fdbc0d00b
Vitor Leonel <falecom@vitorleonel.com>
2019-11-27 11:59:03 -0300

    improve login with buttons

--- htrs/src/pages/Login/index.js
+++ htrs/src/pages/Login/index.js
@@ -1,7 +1,18 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 import { Container } from './styles';
 
 export default function Login() {
-  return <Container />;
+  return (
+    <Container>
+      <Link to="/school" tag="a">
+        Entrar como escola
+      </Link>
+
+      <Link to="/teacher" tag="a">
+        Entrar como palestrante
+      </Link>
+    </Container>
+  );
 }
--- htrs/src/pages/Login/styles.js
+++ htrs/src/pages/Login/styles.js
@@ -3,9 +3,25 @@ import styled from 'styled-components';
 export const Container = styled.div`
   width: 100vw;
   height: 100vh;
-  background-color: red;
+  background-color: #00bcd4;
 
   display: flex;
   justify-content: center;
   align-items: center;
+
+  a {
+    width: fit-content;
+    height: 44px;
+    border-radius: 6px;
+    padding: 0 1rem;
+    border: none;
+    background-color: white;
+    color: #00bcd4;
+    text-decoration: none;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+
+    margin-left: 1rem;
+  }
 `;
